541211074f387afc3425464b1bbf1270589a1792
khalilTaj <khalil.taj.aldean@gmail.com>
2025-08-12 18:54:29 +0300

    added computer choice function

--- code.js
+++ code.js
@@ -1,1 +1,22 @@
-console.log("hello world")
+// console.log("hello world")
+
+const paper = "paper";
+const rock = "rock";
+const scissors = "scissors";
+
+let choice = Math.random();
+// console.log(choice);
+
+function AlgoChoice(){
+    if (choice < 0.33){
+        return(paper);
+    }
+    else if (0.33 < choice && choice < 0.66){
+        return(rock);
+    }
+    else {
+        return(scissors);
+    };
+};
+
+// console.log(AlgoChoice());
