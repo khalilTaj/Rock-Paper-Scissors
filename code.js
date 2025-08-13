1b0e54538ecc87d5e2b7dfde1b4ad5acd85c2df7
khalilTaj <khalil.taj.aldean@gmail.com>
2025-08-13 07:20:21 +0300

    added logic to count rounds till 5 and stops when round played = 5

--- code.js
+++ code.js
@@ -3,6 +3,12 @@
 const paper = "paper";
 const rock = "rock";
 const scissors = "scissors";
+const Hwin = "You won!";
+const Hlose = "You lost..";
+const draw = "Draw!";
+let humanScore = 0;
+let computerScore = 0;
+let roundCounter = 0;
 
 let choice = Math.random();
 // console.log(choice);
@@ -19,10 +25,6 @@ function AlgoChoice(){
     };
 };
 
-// console.log("computer chose: ", AlgoChoice());
-
-let round = prompt("1 for paper | 2 for rock | 3 for scissors");
-
 HumanChoice = function (){
     if (round == "1"){
         return("paper")
@@ -35,34 +37,32 @@ HumanChoice = function (){
     }
 };
 
-// console.log("player chose: ", HumanChoice());
 // console.log(HumanChoice("3"));
-const Hwin = "You won!";
-const Hlose = "You lost..";
-const draw = "Draw!";
+
+
+function playRound (){
+    while (roundCounter < 5){
+        console.log(roundCounter);
+        roundCounter++;
+    }
+    return(roundCounter)
+};
 
 let winner = function() {
-    if(HumanChoice == al)
-}
-
-
-/* let  winner = function (){
-    switch(HumanVsComputer){
-        case draw:
-            HumanChoice() == AlgoChoice()
-            return(console.log("Draw"))
-        case RockWin:
-            HumanChoice == rock && AlgoChoice == scissors
-            return(Hwin)
-        case ScissorsWin:
-            HumanChoice == scissors && AlgoChoice == paper
-            return(Hwin)
-        case PaperWin:
-            HumanChoice == paper && AlgoChoice == rock
-            return(Hwin)
-        default:
+    if(HumanChoice() == AlgoChoice()){
+        return(draw)
+    }
+    else if((HumanChoice() == rock && AlgoChoice() == scissors) ||
+            (HumanChoice() == scissors && AlgoChoice() == paper) ||
+            (HumanChoice() == paper && AlgoChoice() == rock)){
+        return(Hwin)
+    }
+    else{
         return(Hlose)
-    };
-}; */
+    }
+};
 
+// let round = prompt("1 for paper | 2 for rock | 3 for scissors");
+console.log("player chose: ", HumanChoice());
+console.log("computer chose: ", AlgoChoice());
 console.log(winner());
